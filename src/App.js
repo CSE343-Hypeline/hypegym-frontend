3b562e5821bff8864aab94f05377ce92eeb8514e
burcuakar <akarbburcu@gmail.com>
2022-10-25 16:03:56 +0300

    added loginform

--- src/App.js
+++ src/App.js
@@ -13,16 +13,16 @@ function App() {
     <div className="App">
 
       <BrowserRouter>
-        <NavBar/>
+        <NavBar />
         <Routes>
-          <Route exact path="/" element={<HomePage/>}></Route>
-          <Route path="/about" element={<About/>}></Route>
-          <Route path="/contact" element={<Contact/>}></Route>
-          <Route path="/login" element={<Login/>}></Route>
+          <Route exact path="/" element={<HomePage />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/login" element={<Login />}></Route>
 
-        </Routes>  
+        </Routes>
       </BrowserRouter>
-      
+
 
     </div>
   );
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,11 +1,51 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import Loginform from './Loginform';
 function Login() {
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState("");
+
+  const adminUSer = {
+    email: "admin@admin.com",
+    password: "admin123"
+  }
+  const Login = details => {
+    console.log(details);
+
+    if (details.email == adminUSer.email && details.password == adminUSer.password) {
+      console.log("logged in")
+      setUser({
+        name: details.name,
+        email: details.email
+      });
+    }
+    else {
+      console.log("Details do not match")
+      setError("Details do not match")
+    }
+
+  }
+  const Logout = () => {
+    console.log("Logout");
+    setUser({ name: "", email: "" });
+  }
+
   return (
-    <div>
-      Login
+
+    <div className="Login" >
+      {(user.email != "") ? (
+        <div className="welcome">
+          <h2> welcome, <span>{user.name}</span></h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (
+        <Loginform Login={Login} error={error} />
+      )}
+
     </div>
-  )
+
+  );
 }
+
+
 
 export default Login
