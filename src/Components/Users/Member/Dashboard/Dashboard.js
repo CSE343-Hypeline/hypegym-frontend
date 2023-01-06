a37be6fbfa901ff03970a3443d1243a9e529df36
burcuakar <akarbburcu@gmail.com>
2023-01-07 00:01:35 +0300

    changed gymmember webpage

--- src/Components/Users/Member/Dashboard/Dashboard.js
+++ src/Components/Users/Member/Dashboard/Dashboard.js
@@ -5,17 +5,21 @@ import Exercises from "../Exercises/Exercises"
 import "./dashboard.css";
 import MemberNavbar from "./MemberNavbar";
 import Measurement from "../Measurements/Measurement";
-
+import CardDemo from "./CardDemo"
 function DashboardPage() {
   return (
     <div id="main_dashboard">
       <MemberNavbar />
-      <div className="dashboard_graphs">
+      {/* <div className="dashboard_graphs">
         <div className="widgets">
           <div className="first">  <Exercises /></div>
           <div className="second"> <Measurement /></div>
+          <div className="activity"> Your Activities So Far</div>
         </div>
       </div>
+       */}
+      <CardDemo />
+
     </div>
   );
 }
