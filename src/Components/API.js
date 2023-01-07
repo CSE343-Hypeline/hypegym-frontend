226910a510b095aad17ff971bccbc05d4de520a8
sametaslann <abdulsamed418@gmail.com>
2023-01-08 01:24:08 +0300

    test hosting

--- src/Components/API.js
+++ src/Components/API.js
@@ -2,67 +2,79 @@ import axios from "axios";
 
 // Authorizations Control
 export async function apiMe() {
-  const response = await axios.get("http://localhost:8080/api/me");
+  const response = await axios.get(
+    "https://46ab-78-190-132-16.eu.ngrok.io/api/me"
+  );
   return response;
 }
 
 // Login Control
 export async function loginAPI(data) {
-  const response = await axios.post("http://localhost:8080/login", {
-    email: data.email,
-    password: data.password,
-  });
+  const response = await axios.post(
+    "https://46ab-78-190-132-16.eu.ngrok.io/login",
+    {
+      email: data.email,
+      password: data.password,
+    }
+  );
   return response;
 }
 
 // Logout
 export async function LogoutAPI() {
-  const response = await axios.post("http://localhost:8080/api/logout");
+  const response = await axios.post(
+    "https://46ab-78-190-132-16.eu.ngrok.io/api/logout"
+  );
   return response;
 }
 
 // Get User Informations
 export async function getUser(user_id) {
-  const response = await axios.get(`http://localhost:8080/api/user/${user_id}`);
+  const response = await axios.get(
+    `https://46ab-78-190-132-16.eu.ngrok.io/api/user/${user_id}`
+  );
   return response;
 }
 
 // Gets Members of the Gym
 export async function getMembers(gym_id) {
   const response = await axios.get(
-    `http://localhost:8080/api/users/members/${gym_id}`
+    `https://46ab-78-190-132-16.eu.ngrok.io/api/users/members/${gym_id}`
   );
   return response;
 }
 // Gets PTs of the Gym
 export async function getTrainers(gym_id) {
   const response = await axios.get(
-    `http://localhost:8080/api/users/pts/${gym_id}`
+    `https://46ab-78-190-132-16.eu.ngrok.io/api/users/pts/${gym_id}`
   );
   return response;
 }
 
 // Adds member to the Gym
 export async function createUser(user) {
-  const response = await axios.post("http://localhost:8080/api/user", {
-    name: user.name,
-    email: user.email,
-    password: user.password,
-    role: user.role,
-    phone_number: user.phone_number,
-    address: user.address,
-    gym_id: user.gym_id,
-    gender: user.gender,
-    trainer_id: user.trainer.ID,
-    //membership: user.membership.code,
-  });
+  const response = await axios.post(
+    "https://46ab-78-190-132-16.eu.ngrok.io/api/user",
+    {
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      role: user.role,
+      phone_number: user.phone_number,
+      address: user.address,
+      gym_id: user.gym_id,
+      gender: user.gender,
+      trainer_id: user.trainer.ID,
+      //membership: user.membership.code,
+    }
+  );
   return response;
 }
 
 // Gets Members of the Gym
 export async function assignPT(trainerID, memberID) {
   const response = await axios.post(
-    `http://localhost:8080/api/pt/${trainerID}/assign-member`,
+    `https://46ab-78-190-132-16.eu.ngrok.io/api/pt/${trainerID}/assign-member`,
     {
       user_id: memberID,
     }
@@ -73,7 +85,7 @@ export async function assignPT(trainerID, memberID) {
 //Removes member from the Gym
 export async function deleteMember(memberID) {
   const response = await axios.delete(
-    `http://localhost:8080/api/user/${memberID}`
+    `https://46ab-78-190-132-16.eu.ngrok.io/api/user/${memberID}`
   );
   return response;
 }
@@ -81,7 +93,7 @@ export async function deleteMember(memberID) {
 // Get Daily Attedance
 export async function getDailyAttendance(gymId) {
   const response = await axios.get(
-    `http://localhost:8080/api/gym/${gymId}/attendance/day`
+    `https://46ab-78-190-132-16.eu.ngrok.io/api/gym/${gymId}/attendance/day`
   );
   return response;
 }
@@ -89,20 +101,23 @@ export async function getDailyAttendance(gymId) {
 // Get Daily Attedance
 export async function getOnlines(gymId) {
   const response = await axios.get(
-    `http://localhost:8080/api/gym/${gymId}/online`
+    `https://46ab-78-190-132-16.eu.ngrok.io/api/gym/${gymId}/online`
   );
   return response;
 }
 
 // Get Daily Attedance
 export async function contactUs(contact) {
-  const response = await axios.post("http://localhost:8080/contact", contact);
+  const response = await axios.post(
+    "https://46ab-78-190-132-16.eu.ngrok.io/contact",
+    contact
+  );
   return response;
 }
 
 export async function getTrainerOf(memberID) {
   const response = await axios.get(
-    `http://localhost:8080/api/member/get-trainer-of/${memberID}`
+    `https://46ab-78-190-132-16.eu.ngrok.io/api/member/get-trainer-of/${memberID}`
   );
   return response;
 }
